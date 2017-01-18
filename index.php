3083eb8bd9bfb05f8f71ae3484487a70a4f70f15
produktr <produktr@users.noreply.github.com>
2017-01-18 14:14:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ echo	'<style>
 				background-color:#EE7600;
 			}
 			.station.stop{
-				border-top: 1px solid black;
+				border-top: 1px solid dashed;
 				margin-top: 2%;
 				background-color:#DD2724;
 			}
